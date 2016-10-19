843100eada77f6610ae312901befe274695f92c2
Colby-6sco <aalladay@cisco.com>
2016-10-19 22:18:30 +0100

    Update zendesk_bot.py

--- zendesk_bot.py
+++ zendesk_bot.py
@@ -1,27 +1,31 @@
-from flask import Flask , request
+from flask import Flask, request
 import json 
-import urllib , urllib2
+import urllib2
 
 app = Flask(__name__)
+
+BOT_TOKEN = "Your Bot's token"
+ROOM_ID = "Room ID"
 
 @app.route('/', methods =['POST'])
 
 
 def zendesk_payload():
-    
+    events = None
     json_file = request.json
     
     if 'id' in json_file:
         ticket_id = json_file['id']
         ticket_url = json_file['ticket_url']
         ticket_title = json_file['title']
-        events = """Ticket **#%s** has gone stale\n\n%s\n\n%s<br><br>""" % (ticket_id,ticket_url,ticket_title)
-        toSpark(events)
+        events = """Ticket **#{0}** has gone stale\n\n{1}\n\n{2}<br><br>""".format(ticket_id,ticket_url,ticket_title)
         
     elif 'customer_name' in json_file:
         customer_name = json_file['customer_name']
         ticket_url = json_file['ticket_url']
-        events = """**High Priority Customer: %s opened a new ticket**\n\n%s<br><br>""" % (customer_name,ticket_url)
+        events = """**High Priority Customer: {0} opened a new ticket**\n\n{1}<br><br>""".format(customer_name,ticket_url)
+        
+    if events != None:
         toSpark(events)
         
     return 'Ok'
@@ -31,8 +35,8 @@ def zendesk_payload():
 # Function that connects to Spark and sends ticket details in markdown format to a Spark room    
 def toSpark(alerts):
     url = 'https://api.ciscospark.com/v1/messages'
-    headers = {'accept':'application/json','Content-Type':'application/json','Authorization': 'Bearer BOT_TOKEN'}
-    values =   {'roomId':'ROOM_ID', 'markdown': alerts }
+    headers = {'accept':'application/json','Content-Type':'application/json','Authorization': 'Bearer '+ BOT_TOKEN}
+    values =   {'roomId': ROOM_ID, 'markdown': alerts }
     data = json.dumps(values)
     req = urllib2.Request(url = url , data = data , headers = headers)
     response = urllib2.urlopen(req)
@@ -41,4 +45,4 @@ def toSpark(alerts):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' , port=8080, debug= True)
+    app.run(host='0.0.0.0' , port=8080, debug=True)
